777d6360025b848d6bdbd841a984cc1ada5a6b7c
Isaac Watt <isaacwatt@icloud.com>
2018-09-13 11:11:54 -0400

    add realtime editing

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-var showdown = require('showdown');
+var sharejs = require('share');
+var redis = require("redis");
 
 /* use ejs as the view engine */
 app.set('view engine', 'ejs');
@@ -12,6 +13,15 @@ app.use(express.static(__dirname + '/public'));
 /* routes for app */
 app.get('/', function(request, response) {
   response.render('pad');
+});
+
+app.get('/(:id)', function(request, response) {
+  response.render('pad');
+});
+
+/* configure shareJS */
+sharejs.server.attach(app, {
+  db: {type: 'redis'}
 });
 
 /* port 8000 or port used for heroku */
